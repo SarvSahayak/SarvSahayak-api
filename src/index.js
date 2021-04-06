7f9bdea7e7e7d58d2d5b4ca0bb1780866fab45bf
pranshupareek <pranshupareek47@gmail.com>
2021-04-06 19:35:01 +0530

    add cors support

--- src/index.js
+++ src/index.js
@@ -5,18 +5,12 @@ const ngoRouter = require('./routers/ngo')
 const complaintRouter = require('./routers/complaint')
 const app = express()
 const port = process.env.PORT || 3000
+const cors = require('cors');
 
+app.use(cors())
 // app.use((req, res) => {
 //     res.status(503).send('Site under mantainance')
 // })
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
 app.use(express.json())
 app.use(userRouter)
 app.use(ngoRouter)
